96ac77568647456ad0cef94a42102a934105fe8e
duartecancela <duartecancela@gmail.com>
2021-12-03 15:55:09 +0000

    Add controller Parts and migration

---
+++ database/migrations/2021_12_03_151931_create_parts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('stock');
+            $table->longText('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parts');
+    }
+}
---
+++ resources/views/parts/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+    <div class="flex justify-center">
+        <div class="container mx-auto bg-gray-200 p-6 rounded-lg">
+           @foreach($parts as $part)
+               <h1>{{ $part->name }}</h1>
+            @endforeach
+        </div>
+    </div>
+@endsection
+
+
