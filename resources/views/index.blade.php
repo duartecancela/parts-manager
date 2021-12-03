ebcdafe0c1f451d332d283bff42ac8860abca758
duartecancela <duartecancela@gmail.com>
2021-12-03 12:18:06 +0000

    Add header, Content, and Footer Add tailwind css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,9 @@
-<h1>{{config('app.name')}}</h1>
+@extends('layouts.app')
+@section('content')
+    <div class="flex justify-center">
+        <div class="container mx-auto bg-gray-200 p-6 rounded-lg">
+            Home
+        </div>
+    </div>
+@endsection
 
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer bg-white relative pt-1">
+    <div class="container mx-auto px-6">
+        <div class="mt-16 border-t-2 flex flex-col items-center">
+            <div class="sm:w-2/3 text-center py-6">
+                <p class="text-sm text-gray-500 font-bold mb-2">
+                    © {{ date('Y') }} by Duarte Cancela - IPBEJA
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
