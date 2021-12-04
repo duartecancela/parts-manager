6cca7de9aa682c582afb904897252b1a7a7fc03b
Duarte Cancela <duartecancela@gmail.com>
2021-12-04 22:20:38 +0000

    Update create form layout

--- resources/views/parts/create.blade.php
+++ resources/views/parts/create.blade.php
@@ -1,61 +1,70 @@
 @extends('layouts.app')
 @section('content')
     <div class="flex justify-center">
-        <div class="container bg-gray-200 mx-auto p-6 rounded-lg max-w-screen-lg">
-          <p class="text-3xl my-5">Create Electronic Part</p>
-
-          <form class="w-full max-w-sm">
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/3">
-                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name">
-                  Name
-                </label>
-              </div>
-              <div class="md:w-2/3">
-                <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 
-                leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
-                id="name" type="text" value="part name">
-              </div>
+        <div class="container bg-gray-100 mx-auto p-6 rounded-lg max-w-screen-lg">
+        
+            {{-- content name --}}
+            <div class="border-b-2 border-blue-200 mb-8 pb-4">
+                <div class="text-center text-3xl py-2">Create Electronic Part</div>
             </div>
-            <div class="md:flex md:items-center mb-6">
+            
+            {{-- start create form --}}
+            <form class="w-10/12">
+                <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
-                  <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="category">
-                    Category
-                  </label>
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name">
+                    Name
+                    </label>
                 </div>
                 <div class="md:w-2/3">
-                    <div class="inline-block relative w-64">
+                    <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 
+                    leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
+                    id="name" type="text" placeholder="Introduce a part name">
+                </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="category">
+                        Category
+                    </label>
+                    </div>
+                    <div class="md:w-2/3">
                         <select class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 
                         leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
                         @foreach($categories as $category)
-                          <option>{{$category->name}}</option>
+                            <option>{{$category->name}}</option>
                         @endforeach
                         </select>
-                      </div>
+                    </div>
                 </div>
-              </div>
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/3">
-                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="description">
-                  Description
-                </label>
-              </div>
-              <div class="md:w-2/3">
-                <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 
-                leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="description" 
-                type="text" value="part description">
-              </div>
-            </div>
-            <div class="md:flex md:items-center">
-              <div class="md:w-1/3"></div>
-              <div class="md:w-2/3">
-                <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none 
-                text-white font-bold py-2 px-4 rounded" type="button">
-                  Create
-                </button>
-              </div>
-            </div>
-          </form>
+                <div class="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="description">
+                    Description
+                    </label>
+                </div>
+                <div class="md:w-2/3">
+                    <textarea class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 
+                    leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="description" 
+                    type="text" placeholder="Introduce a part description" rows="5"></textarea>
+                </div>
+                </div>
+                <div class="md:flex md:items-center">
+                <div class="md:w-5/6"></div>
+                <div class="md:w-2/3">
+                    <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none 
+                    text-white font-bold py-2 px-6 rounded" type="button">
+                        Create
+                    </button>
+                </div> 
+                <div class="md:w-2/3 md:items-center">
+                    <button class="shadow bg-red-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none 
+                    text-white font-bold py-2 px-6 rounded" type="button">
+                        Delete
+                    </button>
+                </div> 
+                </div>
+            </form>
         </div>
     </div>
 @endsection
--- resources/views/parts/index.blade.php
+++ resources/views/parts/index.blade.php
@@ -2,6 +2,11 @@
 @section('content')
     <div class="flex justify-center">
         <div class="container mx-auto bg-gray-200 p-6 rounded-lg max-w-screen-lg">
+            {{-- content name --}}
+            <div class="border-b-2 border-blue-200 mb-8 pb-4">
+                <div class="text-center text-3xl py-2">List Electronics Parts</div>
+            </div>
+
            @foreach($parts as $part)
                <h1>{{ $part->name }}</h1>
             @endforeach
