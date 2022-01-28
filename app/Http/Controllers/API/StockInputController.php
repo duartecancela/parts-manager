efdd143a2cdbe2f0e481dff74cfdd6a5b3b1c0a8
duartecancela <duartecancela@gmail.com>
2022-01-28 12:20:51 +0000

    finish stock_input e stock_output API documentation

--- app/Http/Controllers/API/StockInputController.php
+++ app/Http/Controllers/API/StockInputController.php
@@ -11,12 +11,86 @@ use App\Http\Resources\StockInput as StockInputResource;
 
 class StockInputController extends BaseController
 {
+    /**
+     * @OA\Get(
+     * path="/api/stock_inputs",
+     * summary="List stock input parts.",
+     * description="List of stock input parts",
+     * operationId="index",
+     * tags={"stock_inputs"},
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="part", type="Object", example={
+     *		 "success": true,
+     *			"data":
+     *				{
+     *					"id": 1,
+     *					"part_id": 2,
+     *					"storage_id": 1,
+     *					"supplier_id": 1,
+     *					"quantity": 5,
+     *					"description": "To new project",
+     *					"created_at": "12/17/2021",
+     *					"updated_at": "12/17/2021"
+     *				},
+     *			"message": "StockInputs fetched."
+     *                     })
+     *        )
+     *     )
+     * )
+     */
     public function index()
     {
         $stockInputs = StockInput::all();
         return $this->sendResponse(StockInputResource::collection($stockInputs), 'StockInputs fetched.');
     }
 
+    /**
+     * @OA\Post(
+     * path="/api/stock_input",
+     * summary="Input part in to stock.",
+     * description="Input stock an electronic part",
+     * operationId="store",
+     * tags={"stock_inputs"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Insert part fields",
+     *    @OA\JsonContent(
+     *       required={"part_id","storage_id", "supplier_id", "quantity"},
+     *       @OA\Property(property="part_id", type="int", example="1"),
+     *       @OA\Property(property="storage_id", type="int",  example="1"),
+     *       @OA\Property(property="supplier_id", type="int", example="1"),
+     *       @OA\Property(property="quantity", type="int", example="5"),
+     *       @OA\Property(property="description", type="string", example="For robotic project"),
+     *    ),
+     * ),
+     * @OA\Response(
+     *     response=201,
+     *     description="Created",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="part", type="Object", example={
+     *                         "success": true,
+     *                          "data": {
+     *                          "id": "10",
+     *                          "part_id": "2",
+     *                          "storage_id": "1",
+     *                          "supplier_id": "1",
+     *                          "quantity": "4",
+     *                          "description": "For robotic project",
+     *                          "created_at": "01/28/2022",
+     *                          "updated_at": "01/28/2022"
+     *                          },
+     *                          "message": "Stock Input created."
+     *                     })
+     *        )
+     *     )
+     * )
+     *
+     *
+     */
     public function store(Request $request)
     {
         $input = $request->all();
--- app/Http/Controllers/API/StockOutputController.php
+++ app/Http/Controllers/API/StockOutputController.php
@@ -10,12 +10,80 @@ use App\Http\Resources\StockOutput as StockOutputResource;
 
 class StockOutputController extends  BaseController
 {
+    /**
+     * @OA\Get(
+     * path="/api/stock_outputs",
+     * summary="List stock output parts.",
+     * description="List of stock output parts",
+     * operationId="index",
+     * tags={"stock_outputs"},
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="part", type="Object", example={
+     *		 "success": true,
+     *			"data":
+     *				{
+     *					"id": 1,
+     *					"part_id": 2,
+     *					"quantity": 5,
+     *					"description": "To new project",
+     *					"created_at": "12/17/2021",
+     *					"updated_at": "12/17/2021"
+     *				},
+     *			"message": "Stock Outputs fetched."
+     *                     })
+     *        )
+     *     )
+     * )
+     */
     public function index()
     {
         $stockOutputs = StockOutput::all();
         return $this->sendResponse(StockOutputResource::collection($stockOutputs), 'Stock Outputs fetched.');
     }
 
+    /**
+     * @OA\Post(
+     * path="/api/stock_output",
+     * summary="Output part in to stock.",
+     * description="Output stock an electronic part",
+     * operationId="store",
+     * tags={"stock_outputs"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Insert part fields",
+     *    @OA\JsonContent(
+     *       required={"part_id", "quantity", "description"},
+     *       @OA\Property(property="part_id", type="int", example="3"),
+     *       @OA\Property(property="quantity", type="int", example="2"),
+     *       @OA\Property(property="description", type="string", example="For robotic project"),
+     *    ),
+     * ),
+     * @OA\Response(
+     *     response=201,
+     *     description="Created",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="part", type="Object", example={
+     *                         "success": true,
+     *                          "data": {
+     *                                  "id": 3,
+     *                                  "part_id": "2",
+     *                                  "quantity": "2",
+     *                                  "description": "For arduino project",
+     *                                  "created_at": "01/28/2022",
+     *                                  "updated_at": "01/28/2022",
+     *                              },
+     *                              "message": "Stock Output created."
+     *                     })
+     *        )
+     *     )
+     * )
+     *
+     *
+     */
     public function store(Request $request)
     {
         $input = $request->all();
