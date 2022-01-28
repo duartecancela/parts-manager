69304eade60ffc9bf3eda6b78f5b1cc11ede3753
duartecancela <duartecancela@gmail.com>
2022-01-28 10:51:36 +0000

    finish auth API documentation

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -11,6 +11,43 @@ use App\Models\User;
 class AuthController extends BaseController
 {
 
+
+    /**
+     * @OA\Get(
+     * path="/api/login",
+     * summary="User login.",
+     * description="User login",
+     * operationId="signin",
+     * tags={"auth"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Insert login fields",
+     *    @OA\JsonContent(
+     *       required={"email", "password"},
+     *
+     *       @OA\Property(property="email", type="string",  example="duarte@mail.pt"),
+     *       @OA\Property(property="password", type="int", example="12345"),
+     *    ),
+     * ),
+     * @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="part", type="Object", example={
+     *                         "success": true,
+     *                         "data": {
+     *                         "token": "3|AGq3cwMXiBoHt84yGuWytjLptSDZ5AU0ZVKIH58d",
+     *                         "name": "Duarte"
+     *                         },
+     *                         "message": "User signed in."
+     *
+     *                     })
+     *        )
+     *     )
+     * )
+     *
+     *
+     */
     public function signin(Request $request)
     {
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
@@ -25,6 +62,43 @@ class AuthController extends BaseController
         }
     }
 
+    /**
+     * @OA\Post(
+     * path="/api/register",
+     * summary="Register user.",
+     * description="register user",
+     * operationId="signup",
+     * tags={"auth"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Insert register fields",
+     *    @OA\JsonContent(
+     *       required={"name","email", "password", "confirm_password"},
+     *       @OA\Property(property="name", type="string", example="Duarte"),
+     *       @OA\Property(property="email", type="string",  example="duarte@mail.pt"),
+     *       @OA\Property(property="passord", type="int", example="12345"),
+     *       @OA\Property(property="confirm_password", type="int", example="12345"),
+     *    ),
+     * ),
+     * @OA\Response(
+     *     response=201,
+     *     description="Created",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="part", type="Object", example={
+     *                         "success": true,
+     *                         "data": {
+     *                         "token": "3|AGq3cwMXiBoHt84yGuWytjLptSDZ5AU0ZVKIH58d",
+     *                         "name": "Duarte"
+     *                         },
+     *                         "message": "User created successfully."
+     *
+     *                     })
+     *        )
+     *     )
+     * )
+     *
+     *
+     */
     public function signup(Request $request)
     {
         $validator = Validator::make($request->all(), [
